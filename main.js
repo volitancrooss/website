67d20c6db1659426fc6a36c793ac62fef19caf36
volitancrooss <spaco7944@gmail.com>
2025-01-21 20:35:27 +0100

    Actualizando: Numero 9 (Pruebas)

--- main.js
+++ main.js
@@ -9,3 +9,26 @@ abrir.addEventListener ("click", () => {
 cerrar.addEventListener ("click", () => {
     nav.classList.remove("visible");
 })
+
+const popup = document.getElementById("popup");
+const closeBtn = document.querySelector(".popup .close");
+const popupMessage = document.getElementById("popup-message");
+// Obtener el contador de visitas del localStorage
+let visitCount = localStorage.getItem("visitCount") || 0;
+visitCount = parseInt(visitCount, 10) + 1;
+
+// Guardar el nuevo contador de visitas en el localStorage
+localStorage.setItem("visitCount", visitCount);
+
+popupMessage.innerHTML = `¡Bienvenido Nuevamente a nuestra web! Has visitado esta página <strong>${visitCount} veces.</strong>`;
+
+// Mostrar el pop-up cada 3 visitas
+if (visitCount % 3 === 0) {
+    popup.style.display = "block";
+    
+}
+
+// Cerrar el pop-up al hacer clic en el botón de cerrar
+closeBtn.addEventListener("click", () => {
+    popup.style.display = "none";
+});
